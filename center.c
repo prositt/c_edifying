b1740cfbdb7057eb207ca6fb57cc1694d5eb6149
Hampus <hfarmangen@gmail.com>
2020-10-11 14:15:14 +0200

    added date function

--- center.c
+++ center.c
@@ -6,5 +6,15 @@ int main(){
 
 	printf("Hello world!\n\n");
 	printf("Now featuring header...\n");
-return(0);
+
+	x = get_date();
+	printf("x = %i\n", x);
+
+	return(0);
+}
+
+int get_date(){
+	int x = 1234;
+	printf("Todays date is %i\n", x);
+	return x;
 }
