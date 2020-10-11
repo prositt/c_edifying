b9c38cd2b05c71c7eb96a5009b8be42f903ceab3
Hampus <hfarmangen@gmail.com>
2020-10-11 15:30:19 +0200

    prints todays date and time

--- center.c
+++ center.c
@@ -3,18 +3,23 @@
 /* Tracks date and time of coding */
 
 int main(){
+	/* Needs to initialize time in a function (not compiler) */
+	time_t t = time(NULL);
+	tm = *localtime(&t);
+
 
 	printf("Hello world!\n\n");
-	printf("Now featuring header...\n");
 
-	printf("intended conflict #2 \n", );
-
+	printf("X -> %i\n", get_date());
 	return(0);
 }
 
 /* This should be added */
 int get_date(){
 	int x = 1234;
-	printf("Todays date is %i\n", x);
+	printf("Year: %d \n", tm.tm_year+1900);
+	printf("Mon: %s \n", months[tm.tm_mon]);
+	printf("Day: %02d \n", tm.tm_mday);
+	printf("Time: %02d:%02d:%02d \n", tm.tm_hour,tm.tm_min, tm.tm_sec);
 	return x;
 }
--- center.h
+++ center.h
@@ -1,4 +1,21 @@
 #include <stdio.h>
+#include <time.h>
 
- int get_date();
+//Structs
+ time_t t;
+ struct tm tm;
+//Constants
  int x;
+ char *months[12] = {"januari",
+                    "februari",
+                    "mars",
+                    "april",
+                    "maj",
+                    "juni",
+                    "augusti",
+                    "september",
+                    "oktober",
+                    "november",
+                    "december"};
+//Functions
+ int get_date();
